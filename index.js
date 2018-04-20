c1520d377d387b921f7b6d4644cfec3bfdb1d71a
Nicolas Duvieusart Déry <nddery@gmail.com>
2018-04-19 22:30:29 -0400

    Use process.env.PORT if available (heroku)

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 require('dotenv').config()
 
 const express = require('express')
-const port = 6440
+const port = process.env.PORT || 6440
 const app = express()
 
 require('./webhooks/github')(app)
 
 app.listen(port, () => {
-  console.log('Server started, listening on port 6440')
+  console.log(`Server started, listening on port ${port}`)
 })
